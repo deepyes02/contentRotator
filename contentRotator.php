56bdae1ed0570e2ec6770f43fa137165790232a5
deepyes02 <deepyes@outlook.com>
2020-12-27 16:18:44 +0545

    barebone directory structure without any code

---
+++ contentRotator.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name: Content Rotator
+ * Description: Rotates Content in a widget, utilizes OOP
+ * Author: Deepesh Dhakal
+ * Author URI: https://www.github.com/deepyes02
+ * License: GPLv3
+ */
